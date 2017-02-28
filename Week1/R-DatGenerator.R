1ef3e49603ef30d812137b7accb7fcfa2fd1b36a
tony.melaragno <melaragnot@gmail.com>
2017-02-27 20:53:39 -0500

    generate data for analysis

---
+++ Week1/R-DatGenerator.R
@@ -0,0 +1,13 @@
+# Purpose: the purpose of this script is to generate data for the 
+#        :  first assignment
+#
+# The data will be outputted as a .dat file formated as follows:
+#
+# index column1 column2
+#
+# index: 1..100
+# column1: random
+# column2: random
+#
+#
+# All of the data will be generated using this R script
